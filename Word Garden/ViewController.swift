2911dbf00d4ddfc6e846eda3d747dc14d1214071
kelvin <chengwj@bc.edu>
2020-02-02 15:24:12 -0500

    Completed 3.3 - The .last String Property and an Intro to Optionals (Values that Might be Nil)

--- Word Garden/ViewController.swift
+++ Word Garden/ViewController.swift
@@ -19,6 +19,8 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        guessLetterButton.isEnabled = false
+        playAgainButton.isHidden = true
     }
     
     func updateUIAfterGuess() {
@@ -32,6 +34,16 @@ class ViewController: UIViewController {
     }
     
     @IBAction func guessLetterFieldChanged(_ sender: UITextField) {
+        if let letterGuessed = guessedLetterField.text?.last
+        {
+            guessedLetterField.text = "\(letterGuessed)"
+            guessLetterButton.isEnabled = true
+        }
+        else
+        {
+            // disable the button if no character in the guessedLetterField
+            guessLetterButton.isEnabled = false
+        }
     }
     
     @IBAction func guessLetterButtonPressed(_ sender: UIButton) {
